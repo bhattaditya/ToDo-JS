e6e11a1f1d46feb8ce255f0ec9c2b37bc7649fd6
Aditya Bhatt <bhattaditya2@gmail.com>
2024-04-06 23:07:49 +0530

    refactor

--- app.js
+++ app.js
@@ -5,57 +5,64 @@ const taskItems = document.querySelector(".task-items");
 //on page refresh loads data from local storage
 showTasks();
 
-inputBox.addEventListener("keyup", function(e) {
-    if (e.code === 'Enter') {
-        if (inputBox.value.trim().length == 0) {
-            alert ("Please enter a task!");
-        } 
-    }
-});
-
-//  on click event - user input will be saved in browser local storage 
-addBtn.onclick = () => {
-    let userData = inputBox.value;
-    console.log(userData.trim().length);
-    if (userData.trim().length == 0) {
-        alert("Please enter task!");
-    } else {
-        let getLocalStorage = localStorage.getItem("New Todo");
-        if (getLocalStorage == null) {
-            arr = [];
-        } else {
-            arr = JSON.parse(getLocalStorage);
-        }
-        arr.push(userData);
-        localStorage.setItem("New Todo", JSON.stringify(arr));
-        showTasks();
-    }
-}
-
 // will fetch records from local storage and add them as inside ul as li tags
 function showTasks() {
-    let getLocalStorage = localStorage.getItem("New Todo");
-    if (getLocalStorage == null) {
-        arr = [];
-    } else {
-        arr = JSON.parse(getLocalStorage);
-    }
-    let newLiTag = '';
-    arr.forEach((element, index) => {
-        newLiTag += `<li> ${element} <span onclick="deleteTask(${index})" ><i class="fa fa-remove"></i></span></li>`
-    });
-    
-    taskItems.innerHTML = newLiTag;
-    inputBox.value = '';
+  checkStorage();
+  let newLiTag = "";
+  arr.forEach((element, index) => {
+    newLiTag += `<li> ${element} <span onclick="deleteTask(${index})" ><i class="fa fa-remove"></i></span></li>`;
+  });
+
+  taskItems.innerHTML = newLiTag;
+  inputBox.value = "";
 }
+
+// saves user input in browser local storage
+function save() {
+  let userData = inputBox.value;
+  if (userData.trim().length == 0) {
+    alert("Please enter task!");
+  } else {
+    checkStorage();
+    arr.push(userData);
+    localStorage.setItem("New Todo", JSON.stringify(arr));
+    return arr;
+  }
+}
+
+function checkStorage() {
+  let getLocalStorage = localStorage.getItem("New Todo");
+  if (getLocalStorage == null) {
+    arr = [];
+  } else {
+    arr = JSON.parse(getLocalStorage);
+  }
+}
+
+// handles input event like click and keypress
+function handleInput(event) {
+  if (
+    event.type === "click" ||
+    (event.type === "keypress" && event.key === "Enter")
+  ) {
+    save();
+    showTasks();
+  }
+}
+
+addBtn.addEventListener("click", handleInput);
+inputBox.addEventListener("keypress", handleInput);
+
+// inputBox.addEventListener("keypress", function (event) {
+//   if (event.key === "Enter") {
+//     save();
+//   }
+// });
 
 // delete task items
 function deleteTask(index) {
-    let getLocalStorage = localStorage.getItem("New Todo");
-    let arr = JSON.parse(getLocalStorage);
-
-    arr.splice(index, 1);
-    localStorage.setItem("New Todo", JSON.stringify(arr));
-    showTasks();
+  checkStorage();
+  arr.splice(index, 1);
+  localStorage.setItem("New Todo", JSON.stringify(arr));
+  showTasks();
 }
-
